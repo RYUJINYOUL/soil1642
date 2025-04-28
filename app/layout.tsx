7755c5ef7389084a764b8ca58ffd93cbd40307f6
youssi <zzz123zz12@naver.com>
2025-04-29 00:23:05 +0900

    floating

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/themeProvider";
 import FloatingMenu from "@/components/ui/FloatingMenu"
+import FabButton from '@/components/ui/FabButton';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,7 +30,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-            <FloatingMenu />
+            <FabButton />
           </body>
     </html>
   );
